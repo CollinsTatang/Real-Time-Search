d48f3daee3708dafce8f7aeeda6f7a4c3dccd4b5
Collins Tatang <makungongcollins@gmail.com>
2022-12-03 05:51:19 +0100

    Add User and Article Associations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:show, :index]
 
   # GET /articles or /articles.json
   def index
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+
+    belongs_to :user
+    
 end
