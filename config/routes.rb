bf0037cf2dad7d140afe24d97bf089b146edeb36
Collins Tatang <makungongcollins@gmail.com>
2024-06-12 09:21:31 +0100

    Remove devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
-  resources :articles
-  root "articles#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html 
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
--- db/migrate/20221202131019_create_articles.rb
+++
@@ -1,9 +0,0 @@
-class CreateArticles < ActiveRecord::Migration[7.0]
-  def change
-    create_table :articles do |t|
-      t.string :title
-
-      t.timestamps
-    end
-  end
-end
