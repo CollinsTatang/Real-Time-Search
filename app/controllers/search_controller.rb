b1ec29a960adcef6e2c28564e90fa22aaaecff57
Collins Tatang <makungongcollins@gmail.com>
2024-07-05 13:22:14 +0100

    Fix linter errors

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,16 @@
 class SearchController < ApplicationController
-    def index
-        query = params[:query]
-        articles = Article.new(title: query)
-        articles.save
-        @results = Article.where('title ILIKE ?', "%#{query}%")
+  def index
+    query = params[:query]
+    articles = Article.new(title: query)
+    articles.save
+    @results = Article.where('title ILIKE ?', "%#{query}%")
 
-        user_search_details = UserSearchAnalytic.new(record: query, ip_details: request.remote_ip)
-        user_search_details.save
+    user_search_details = UserSearchAnalytic.new(record: query, ip_details: request.remote_ip)
+    user_search_details.save
 
-        respond_to do |format| 
-            format.json { render json: @results.as_json(only: [:title]) }
-            format.html { render :index }
-        end
-    end      
+    respond_to do |format|
+      format.json { render json: @results.as_json(only: [:title]) }
+      format.html { render :index }
+    end
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,3 @@
 class Article < ApplicationRecord
-    validates :title, presence: true
+  validates :title, presence: true
 end
