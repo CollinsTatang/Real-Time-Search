f29ebed4f0a707eda43d36fc058de0e0f9d421df
Collins Tatang <makungongcollins@gmail.com>
2024-07-05 13:11:49 +0100

    Create user search details and article

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,16 @@
 class SearchController < ApplicationController
     def index
-        record = params[:record]
-        user_ip = request.remote_ip
-        @results = Article.where('title ILIKE ?', "%#{record}%")
+        query = params[:query]
+        articles = Article.new(title: query)
+        articles.save
+        @results = Article.where('title ILIKE ?', "%#{query}%")
 
-        user_search_details = UserSearchAnalytic.new(record: record, ip_details: request.remote_ip)
+        user_search_details = UserSearchAnalytic.new(record: query, ip_details: request.remote_ip)
         user_search_details.save
 
-        respond_to do |format|
-            format.html { render :index } # Render the HTML view as before
-            format.json { render json: @results } # Render the results as JSON
-          end
-      end      
+        respond_to do |format| 
+            format.json { render json: @results.as_json(only: [:title]) }
+            format.html { render :index }
+        end
+    end      
 end
