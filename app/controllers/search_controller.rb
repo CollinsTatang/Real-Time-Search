02338888036fe0fb22c492bb15505ccefb2a4f26
Collins Tatang <makungongcollins@gmail.com>
2024-07-06 17:21:35 +0100

    Update javascript to send the final instance

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,28 @@
 class SearchController < ApplicationController
+  protect_from_forgery except: :index
+
   def index
     query = params[:query]
-    articles = Article.new(title: query)
-    articles.save
-    @results = Article.where('title ILIKE ?', "%#{query}%")
 
-    user_search_details = UserSearchAnalytic.new(record: query, ip_details: request.remote_ip)
-    user_search_details.save
+    if query.present? && query.match?(/\b\w+\z/)
+
+      articles = Article.new(title: query)
+      articles.save
+
+      @results = Article.where('title ILIKE ?', "%#{query}%")
+
+      user_search_details = UserSearchAnalytic.new(record: query, ip_details: request.remote_ip)
+      user_search_details.save
+    else
+      @results = []
+    end
 
     respond_to do |format|
       format.json { render json: @results.as_json(only: [:title]) }
-      format.html { render :index }
+      format.html do
+        flash.now[:notice] = "No results found for '#{query}'" if @results.empty? && query.present?
+        render :index
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/search', to: 'search#index'
+  post '/search', to: 'search#index'
   get '/analytics', to: 'analytics#index'
 end
